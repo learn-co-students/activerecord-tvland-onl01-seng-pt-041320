882e4d38ed271f83334e3a48f5d4cfe40da69f75
baker615512 <baker615512@gmail.com>
2020-06-27 21:02:11 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        roles = Character.where(actor_id: self.id)
+        roles.collect do |i|
+            "#{i.name} - #{i.show.name}"
+        end
+    end
   
 end
