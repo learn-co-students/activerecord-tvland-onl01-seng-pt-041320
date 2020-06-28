b738bef7ed84485533dc1266e463ffc3ea76f8dc
dnodolf1212 <dnodolf@gmail.com>
2020-06-27 17:25:34 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  attr_accessor :first_name, :last_name
+  
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    result = self.characters.map(&:name) + self.shows.map(&:name)
+    result.join(" - ") 
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  attr_accessor :catchphrase
+
+    def initialize(catchphrase)
+      @catchphrase = catchphrase 
+    end 
+
 end
