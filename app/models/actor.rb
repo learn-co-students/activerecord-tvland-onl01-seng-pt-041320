ab753852b198416b357f01582d74434d068f7716
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-06-30 15:40:43 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        character = Character.where(actor_id: self.id)
+        show = Show.where(id: character[0].show_id)
+        character[0].name + " - " + show[0].name
+    end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        list = []
+        character = Character.where(show_id: self.id)
+        actor = Actor.where(id: character[0].actor_id)
+        fullname = actor[0].first_name + " " + actor[0].last_name
+        list << fullname
+    end
   
 end
