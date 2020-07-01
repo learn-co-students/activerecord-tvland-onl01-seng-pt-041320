2fb7a7c0123436da47ac338d33f594606c296efc
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-30 21:30:25 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,12 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def actors_list
+    shows = Character.where(show_id: self.id)
+    shows.map do |x|
+        x.actor.first_name + x.actor.last_name
+    end
   end
 end
