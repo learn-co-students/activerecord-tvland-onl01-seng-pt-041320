a1adc1d7844b23d96365b34a90b9189fd72f85be
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-07 23:56:34 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,12 @@ class Actor < ActiveRecord::Base
     has_many :shows, through: :characters
 
     def full_name
-        # actor = Actor.create(:first_name, :last_name)
+        Actor.all.map {|t| "#{t.first_name} #{t.last_name}" }.join("")
+    end
+
+    def list_roles
+        #  can list all of its shows and characters
+        characters.map{|c|"#{c.name} - #{c.show.name}"}.join("")
     end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,9 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+
+  def actors_list
+    # returns a list of the full names of each actor associated with the show
+    self.actors.map{|a| "#{a.first_name} #{a.last_name}"}    
+  end
 end
