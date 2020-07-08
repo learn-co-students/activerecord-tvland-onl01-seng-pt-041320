40b32804b7f67d9f8a33d439c8acd9bc95bb9fad
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-07 20:44:54 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,9 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        # actor = Actor.create(:first_name, :last_name)
+    end
   
 end
